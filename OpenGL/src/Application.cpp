46f793d0662edb1f3d922f8ff9e10ec1aef072db
brousserie <b.rousserie24@gmail.com>
2019-09-29 07:26:09 +0200

    glm and orthogonal matrix projection

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -11,7 +11,8 @@
 #include "Shader.h"
 #include "Texture.h"
 
-
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 
 int main()
@@ -68,12 +69,15 @@ int main()
 		va.AddBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
+
+		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f);
 		
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.1f, 0.2f, 0.4f, 1.0f);
+		shader.SetUniformMat4f("u_MVP", proj);
 
-		Texture texture("res/textures/Nioh_Logo.png");
+		Texture texture("res/textures/cpp_logo.png");
 		texture.Bind();
 		shader.SetUniform1i("u_Texture", 0);
 		
